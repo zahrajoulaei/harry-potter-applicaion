8ad7ad492c9f06d6a841d72c8fe445c4878080de
ZahraJou <zahrajoulaei@gmail.com>
2024-06-22 20:53:34 -0400

    add the click handler for cards

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const charactersContainer = document.getElementById("charactersContainer");
   const searchButton = document.getElementById("search");
   const searchInput = document.getElementById("searchInput");
+  const selectedChar = document.getElementById("selectedChar");
 
   searchButton.addEventListener("click", async (event) => {
     event.preventDefault();
@@ -12,23 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
     if (query) {
       const characters = await fetchCharacters(query);
       displayCharacters(characters);
-      console.log(characters);
+      console.log("characters:", characters);
+    }
+    console.log("characters:", characters);
+  });
+  searchInput.addEventListener("keypress", async (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const query = searchInput.value.trim();
+      console.log("query is:", query);
+      if (query) {
+        const characters = await fetchCharacters(query);
+        displayCharacters(characters);
+        console.log(characters);
+      }
     }
   });
-  searchInput.addEventListener('keypress', async (event) => {
-    if(event.key === 'Enter'){
-        event.preventDefault();
-        const query = searchInput.value.trim();
-        console.log("query is:", query);
-        if (query) {
-          const characters = await fetchCharacters(query);
-          displayCharacters(characters);
-          console.log(characters);
-        }
-    }
-
-  });
-
 
   async function initialLoad() {
     const characters = await fetchCharacters();
@@ -53,18 +53,33 @@ document.addEventListener("DOMContentLoaded", () => {
       characterFullname.textContent = character.fullName;
       characterDiv.appendChild(characterFullname);
 
-      if (character.house) {
-        const characterHouse = document.createElement("p");
-        characterHouse.textContent = `House: ${character.house}`;
-        characterDiv.appendChild(characterHouse);
-      }
-
       if (character.image) {
         const characterImage = document.createElement("img");
         characterImage.src = character.image;
         characterImage.alt = `${character.name} image`;
         characterDiv.appendChild(characterImage);
       }
+
+      const clickHandler = () => {
+        const characterBoard = document.createElement("div");
+        characterBoard.classList.add("characterBoard");
+
+        const houseElement = document.createElement("h4");
+        houseElement.textContent = `Hogwarts House: ${character.hogwartsHouse}`;
+        characterBoard.appendChild(houseElement);
+
+        if (character.children && character.children.length > 0) {
+          const childrenElement = document.createElement("h4");
+          childrenElement.textContent = `Children: ${character.children}`;
+          characterBoard.appendChild(childrenElement);
+        }
+
+
+        selectedChar.appendChild(characterBoard);
+        characterDiv.removeEventListener("click", clickHandler);
+      };
+
+      characterDiv.addEventListener("click", clickHandler);
 
       charactersContainer.appendChild(characterDiv);
     });
